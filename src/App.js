6721037f1f830c6c3258dad035f875837dcefbee
Paola Garay <garay.angela@gmail.com>
2020-09-21 23:16:30 +0200

    Dropdown item selection component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
-//import Accordion from './components/Accordion'
-import Search from './components/Search'
+import React, { useState } from 'react'
+//import Accordion from './components/Accordion';
+//import Search from './components/Search';
+import Dropdown from './components/Dropdown'
 
 const items = [
   {
@@ -15,13 +16,36 @@ const items = [
     title: 'How do you use React?',
     content: 'You use React by creating components'
   }
-]
+];
+
+
+const options = [
+  {
+      label: 'The Color Red',
+      value: 'red'
+  }, 
+  {
+      label: 'The Color Green',
+      value: 'green'
+  }, 
+  {
+      label: 'A Shade of Blue',
+      value: 'blue'
+  } 
+];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
           {/* <Accordion items={items}/> */}
-          <Search />
+          {/* <Search /> */}
+          <Dropdown 
+            options = {options} 
+            selected = {selected}
+            onSelectedChange = {setSelected}
+          />
     </div>
   );
 }
